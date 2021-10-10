bd5e485c640bb6373c7ed743c400fb49c3bb9f12
Miriam Alex <dishsoap.mag@gmail.com>
2021-10-10 13:20:55 -0400

    updating for issue 3

--- src/pages/issues.js
+++ src/pages/issues.js
@@ -14,98 +14,85 @@ const Issues = () => {
     <>
       <MobileNav />
       <Nav />
-      <Subheader title="issue two" />
+      <Subheader title="issue three" />
       <Layout>
         <Head title="issues" />
         <div className={layoutStyles.mission}>
           <h1 className={issueStyles.heading}>poetry</h1>
           <div className={issueStyles.parent}>
             <div className={issueStyles.bottomMargin}>
-              <h2>charlie blodnieks</h2>
-              <Link to="/issue2/ode-to-desperate-housewives">
-                <p>Ode to Desperate Housewives</p>
+
+              <h2>sunny vuong</h2>
+              <Link to="/issue3/frankievalli">
+                <p>Frankie Valli’s in love with you</p>
               </Link>
-              <h2>alex dang</h2>
-              <Link to="/issue2/mac-miller-georgia-o-keeffe-collaboration-track">
-                <p>Mac Miller &amp; Georgia O’Keeffe COLLABORATION TRACK</p>
+              <h2>josephine wu</h2>
+              <Link to="/issue3/waterprayers">
+                <p>Water Prayers</p>
               </Link>
-              <h2>kavi kshiraj</h2>
-              <Link to="/issue2/constructed-grief">
-                <p>Constructed Grief</p>
+              <h2>ashley hajimirsadeghi</h2>
+              <Link to="/issue3/alternateuniverse">
+                <p>IN AN ALTERNATE UNIVERSE WE EAT CAKE</p>
               </Link>
+
             </div>
+
             <div className={issueStyles.bottomMargin}>
-            <h2>norah brady</h2>
-              <Link to="/issue2/beowulf">
-                <p>beowulf</p>
+            <h2>sal kang</h2>
+              <Link to="/issue3/theorange">
+                <p>The Orange Has Body Image Issues</p>
               </Link>
-            <h2>self insert</h2>
-              <Link to="https://www.youtube.com/watch?v=p2-7zlPup38">
-                <p>hi yongsie</p>
+            <h2>ds maolalai</h2>
+              <Link to="/issue3/theleavesaregreen">
+                <p>And the leaves are green</p>
               </Link>
-              <h2>rhiannon mcgavin</h2>
-              <Link to="/issue2/pith">
-                <p>Pith</p>
+            <h2>ashley hajimirsadeghii</h2>
+              <Link to="/issue3/besweet">
+                <p>Be Sweet</p>
               </Link>
-              <Link to="/issue2/empty">
-                <p>Empty</p>
-              </Link>
-              <h2>rl wheeler</h2>
-              <Link to="/issue2/ars-poetica-open-composition">
-                <p>Ars Poetica: Open Composition</p>
-              </Link>
+
             </div>
           </div>
+
           <h1 className={issueStyles.heading}>prose</h1>
           <div className={issueStyles.parent}>
             <div className={issueStyles.bottomMargin}>
-              <h2>martin greenacre</h2>
-              <Link to="/issue2/sunshine-on-marchmont">
-                <p>Sunshine on Marchmont</p>
+
+              <h2>salonee verma</h2>
+              <Link to="/issue3/tobeclear">
+                <p>To Be Clear: A Femme Fatale (Gender-Neutral!) Guides You Through A Noir Film in Bollywood (First Of Its Kind! Tickets Open Now!)</p>
               </Link>
-              <h2>blake chernin</h2>
-              <Link to="/issue2/typhoid-mary">
-                <p>Typhoid Mary Would Like to Address You From Her Lonely Bungalow on North Brother Island, Where She Was Imprisoned During the Years 1907-1910 and Again from 1915 Until Her Death in 1938</p>
-              </Link>
+
             </div>
             <div>
-              <h2>norah brady</h2>
-              <Link to="/issue2/time-bomb">
-                <p>Time Bomb</p>
+
+              <h2>dedeepya guthikonda</h2>
+              <Link to="/issue3/hirasdaycare">
+                <p>Mrs. Hira's Daycare</p>
               </Link>
+
             </div>
           </div>
-          <h1 className={issueStyles.heading}>play</h1>
-          <div className={issueStyles.parent}>
-            <div className={issueStyles.bottomMargin}>
-              <h2>julia beecher</h2>
-              <Link to="/issue2/end-of-days">
-                <p>End of Days</p>
-              </Link>
-            </div>
-          </div>
+
           <h1 className={issueStyles.heading}>art</h1>
           <div className={issueStyles.parent}>
             <div>
-              <Link to="/issue2/fabrice-poussin">
-                <h2>fabrice poussin</h2>
-                <p>Yet Another Story</p>
-                <p>Modern Couple</p>
-                <p>Illusion</p>
+              <Link to="/issue3/darcymariemelton">
+                <h2>darcy marie melton</h2>
+                <p>Kingfisher</p>
               </Link>
-              <Link to="/issue2/carly-chan">
-                <h2>carly chan</h2>
-                <p>Trichotomy</p>
+              <Link to="/issue3/bidingtime">
+                <h2>grace joylynn anderson</h2>
+                <p>biding time until the spring arrives</p>
               </Link>
             </div>
             <div>
-            <Link to="/issue2/adina-guo">
-                <h2>adina guo</h2>
-                <p>Wai Gong</p>
+            <Link to="/issue3/melodyserra">
+                <h2>melody serra</h2>
+                <p>In the company of trees</p>
               </Link>
           </div>
         </div>
-
         </div>
       </Layout>
     </>

d2fe8d3dc14b07ee3f50ec94688bd19c48f24289
Miriam Alex <dishsoap.mag@gmail.com>
2022-05-29 15:52:07 -0400

    changing archive

--- src/pages/archive.js
+++ src/pages/archive.js
@@ -18,6 +18,93 @@ const Masthead = () => {
     <Layout>
       <Head title="archive" />
 
+      <h1 className={issueStyles.heading}>issue 3: september 2020</h1>
+      <div className={layoutStyles.mission}>
+          <h1 className={issueStyles.heading}>poetry</h1>
+          <div className={issueStyles.parent}>
+            <div className={issueStyles.bottomMargin}>
+
+              <h2>sunny vuong</h2>
+              <Link to="/issue3/frankievalli">
+                <p>Frankie Valli’s in love with you</p>
+              </Link>
+              <h2>sal kang</h2>
+              <Link to="/issue3/sonnetforbirdsofparadise">
+                <p>sonnet for birds of paradise</p>
+              </Link>
+              <h2>josephine wu</h2>
+              <Link to="/issue3/waterprayers">
+                <p>Water Prayers</p>
+              </Link>
+              <h2>ashley hajimirsadeghi</h2>
+              <Link to="/issue3/alternateuniverse">
+                <p>IN AN ALTERNATE UNIVERSE WE EAT CAKE</p>
+              </Link>
+
+            </div>
+
+
+            <div className={issueStyles.bottomMargin}>
+
+            <Link to="/issue3/bidingtime">
+              <h2>grace joylynn anderson</h2>
+              <p>biding time until the spring arrives</p>
+            </Link>
+
+            <h2>sal kang</h2>
+              <Link to="/issue3/theorange">
+                <p>The Orange Has Body Image Issues</p>
+              </Link>
+            <h2>ds maolalai</h2>
+              <Link to="/issue3/theleavesaregreen">
+                <p>And the leaves are green</p>
+              </Link>
+            <h2>ashley hajimirsadeghii</h2>
+              <Link to="/issue3/besweet">
+                <p>Be Sweet</p>
+              </Link>
+
+            </div>
+          </div>
+
+          <h1 className={issueStyles.heading}>prose</h1>
+          <div className={issueStyles.parent}>
+            <div className={issueStyles.bottomMargin}>
+
+              <h2>salonee verma</h2>
+              <Link to="/issue3/tobeclear">
+                <p>To Be Clear: A Femme Fatale (Gender-Neutral!) Guides You Through A Noir Film in Bollywood (First Of Its Kind! Tickets Open Now!)</p>
+              </Link>
+
+            </div>
+            <div>
+
+              <h2>dedeepya guthikonda</h2>
+              <Link to="/issue3/hirasdaycare">
+                <p>Mrs. Hira's Daycare</p>
+              </Link>
+
+            </div>
+          </div>
+
+          <h1 className={issueStyles.heading}>art</h1>
+          <div className={issueStyles.parent}>
+            <div>
+              <Link to="/issue3/darcymariemelton">
+                <h2>darcy marie melton</h2>
+                <p>Kingfisher</p>
+              </Link>
+
+            </div>
+            <div>
+            <Link to="/issue3/melodyserra">
+                <h2>melody serra</h2>
+                <p>In the company of trees</p>
+              </Link>
+          </div>
+        </div>
+        </div>
+
       <h1 className={issueStyles.heading}>issue 2: june 2021</h1>
       <div className={layoutStyles.mission}>
         <h1 className={issueStyles.heading}>poetry</h1>

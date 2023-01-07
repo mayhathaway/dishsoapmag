171de9b9e573c65f27ad31a48b0bbbc206e8330b
miriam-alex <dishsoap.mag@gmail.com>
2023-01-07 10:33:03 -0500

    new issue

--- src/pages/archive.js
+++ src/pages/archive.js
@@ -18,6 +18,80 @@ const Masthead = () => {
     <Layout>
       <Head title="archive" />
 
+      <h1 className={issueStyles.heading}> issue 4 </h1>
+        <div className={layoutStyles.mission}>
+            <h1 className={issueStyles.heading}>poetry</h1>
+            <div className={issueStyles.parent}>
+              <div className={issueStyles.bottomMargin}>
+
+                <h2>elliot lee</h2>
+                <Link to="/issue4/to-term">
+                  <p>to term</p>
+                </Link>
+
+                <h2>daniel boyko</h2>
+                <Link to="/issue4/to-my-future-nephew">
+                  <p>To My Future Nephew II</p>
+                </Link>
+
+                <h2>anna feng</h2>
+                <Link to="/issue4/a-page-from-her-book">
+                  <p>a page from her book</p>
+                </Link>
+              </div>
+
+
+              <div className={issueStyles.bottomMargin}>
+
+                <h2>matt hsu</h2>
+                <Link to="/issue4/32lemons">
+                  <p>32 lemons</p>
+                </Link>
+              
+                <h2>courtney felle</h2>
+                <Link to="/issue4/teens-on-tiktok">
+                  <p>teens on tiktok think i should set boundaries but i am</p>
+                </Link>
+
+                <h2>leela raj-sankar</h2>
+                <Link to="/issue4/the-world-is-ending-and-i-am-buying-groceries">
+                  <p>The World Is Ending And I Am Buying Groceries</p>
+                </Link>
+
+              </div>
+            </div>
+
+            <h1 className={issueStyles.heading}>prose</h1>
+            <div className={issueStyles.parent}>
+              <div className={issueStyles.bottomMargin}>
+
+                <h2>will musgrove</h2>
+                <Link to="/issue4/lawn-chair-larry">
+                  <p>Lawn Chair Larry</p>
+                </Link>
+
+                <h2>nora sun</h2>
+                <Link to="/issue4/absurdum">
+                  <p>Absurdum</p>
+                </Link>
+
+              </div>
+              <div>
+
+                <h2>ashley gilland</h2>
+                <Link to="/issue4/the-chocolatier">
+                  <p>The Chocolatier</p>
+                </Link>
+
+                <h2>E.J. Nash</h2>
+                <Link to="/issue4/henry-wilson">
+                  <p>Henry Wilson and the Center of the Sun</p>
+                </Link>
+
+              </div>
+            </div>
+          </div>
+
       <h1 className={issueStyles.heading}>issue 3: september 2020</h1>
       <div className={layoutStyles.mission}>
           <h1 className={issueStyles.heading}>poetry</h1>
--- src/pages/issues.js
+++ src/pages/issues.js
@@ -22,68 +22,58 @@ const Issues = () => {
           <div className={issueStyles.parent}>
             <div className={issueStyles.bottomMargin}>
 
-              <h2>elliot lee</h2>
-              <Link to="/issue4/to-term">
-                <p>to term</p>
+              <h2>howie good</h2>
+              <Link to="/issue5/the-anxiety-index">
+                <p>The Anxiety Index</p>
               </Link>
 
-              <h2>daniel boyko</h2>
-              <Link to="/issue4/to-my-future-nephew">
-                <p>To My Future Nephew II</p>
+              <h2>jeffrey letterly</h2>
+              <Link to="/issue5/swirling">
+                <p>Swirling</p>
               </Link>
 
-              <h2>anna feng</h2>
-              <Link to="/issue4/a-page-from-her-book">
-                <p>a page from her book</p>
+              <h2>howie good</h2>
+              <Link to="/issue5/the-day-before-yesterday">
+                <p>The Day Before Yesterday</p>
               </Link>
             </div>
 
 
             <div className={issueStyles.bottomMargin}>
 
-              <h2>matt hsu</h2>
-              <Link to="/issue4/32lemons">
-                <p>32 lemons</p>
+              <h2>nathan lee</h2>
+              <Link to="/issue5/spring-meditation">
+                <p>spring meditation</p>
               </Link>
             
-              <h2>courtney felle</h2>
-              <Link to="/issue4/teens-on-tiktok">
-                <p>teens on tiktok think i should set boundaries but i am</p>
+              <h2>ashley kim</h2>
+              <Link to="/issue5/mom-and-dad">
+                <p>Mom and Dad</p>
               </Link>
 
-              <h2>leela raj-sankar</h2>
-              <Link to="/issue4/the-world-is-ending-and-i-am-buying-groceries">
-                <p>The World Is Ending And I Am Buying Groceries</p>
+              <h2>julieanne larick</h2>
+              <Link to="/issue5/the-anxious-girlfriends-unruly-skeletons">
+                <p>The Anxious Girlfriend's Unruly Skeletons</p>
               </Link>
 
             </div>
           </div>
 
-          <h1 className={issueStyles.heading}>prose</h1>
+          <h1 className={issueStyles.heading}>art</h1>
           <div className={issueStyles.parent}>
             <div className={issueStyles.bottomMargin}>
 
-              <h2>will musgrove</h2>
-              <Link to="/issue4/lawn-chair-larry">
-                <p>Lawn Chair Larry</p>
-              </Link>
-
-              <h2>nora sun</h2>
-              <Link to="/issue4/absurdum">
-                <p>Absurdum</p>
+              <h2>theresa kohlbeck jakobsen</h2>
+              <Link to="/issue5/skilnadarotti">
+                <p>skilnadarótti</p>
               </Link>
 
             </div>
             <div>
 
-              <h2>ashley gilland</h2>
-              <Link to="/issue4/the-chocolatier">
-                <p>The Chocolatier</p>
-              </Link>
-
-              <h2>E.J. Nash</h2>
-              <Link to="/issue4/henry-wilson">
-                <p>Henry Wilson and the Center of the Sun</p>
+              <h2>james diaz</h2>
+              <Link to="/issue5/words-get-in-the-way">
+                <p>Words Get In The Way</p>
               </Link>
 
             </div>

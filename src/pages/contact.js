a8765afbc1e5e78a2eeefe7dfb3211d6fcba72cc
Kelzaam <that1poetrynerd@gmail.com>
2024-06-04 11:38:35 -0700

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -46,7 +46,7 @@ const ContactPage = () => {
               <label>
                 <p>type your message here...</p>{" "}
                 <textarea
-                  placeholder="I love rubbing copious amounts of dish soap on..."
+                  placeholder="I love dumping copious amounts of dish soap on..."
                   name="message"
                   className={contactStyles.boxsizingBorder}
                 ></textarea>

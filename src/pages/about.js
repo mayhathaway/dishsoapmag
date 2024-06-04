32eea26c0f1195f6b80321091d6d39f79ad0a95e
Kelzaam <that1poetrynerd@gmail.com>
2024-06-04 10:36:29 -0700

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -24,39 +24,17 @@ const AboutPage = () => {
           </div>
           <div className={aboutStyles.pinkLineTop}></div>
           <div className={aboutStyles.mission}>
-            <h2>
-              <center>mission statement</center>
-            </h2>
             <p>
-              Dishsoap wasn't founded by a group of teenagers hailing from all over
-              the world. In our various forays into the literary world, we have
-              all witnessed or experienced our fair share of elitism and
-              professionalism. Dishsoap strives to be the antithesis of that
-              exclusivity. So often the world of publication seems difficult,
-              strict, and harsh; Dishsoap is the opposite of all of this - it’s
-              goofy and unpretentious and pokes fun at itself. It makes terrible
-              puns. It doesn’t try to be anything other than what it is.
+             <strong>dish soap</strong> /ˈdɪʃ ˌsoʊp/ (noun) </p> 
+            <p> a thick liquid that is often blue, white, or green. commonly found in or near sinks. used for 
+            cleaning dishes and other things commonly found in kitchens like pots, pans, fish spatulas, 
+            egg cubers, shrimp deveiners, condiment guns, and corn peelers.
             </p>
-            <p>
-              Here at Dishsoap, one of the best things is that we disagree on
-              ideas. We argue over organization and take votes on editing
-              strategies. Instead of pigeonholing, we truly accept and consider
-              anything and everything, no matter how outrageous or spunky.
-              Dishsoap doesn’t try to fight for the lowest acceptance rates; we
-              cater to everyone, whether you’re a musician who plays songs at
-              live events or a teenager who writes poems at midnight in their
-              bedroom.
-            </p>
-            <p>
-              We are interested in the young, the
-              perhaps-not-completely-finished, the work-in-progress. Inclusivity
-              and openness is at the core of Dishsoap’s values. Not easy, but
-              for the diligent, not lenient, but kind. Too often people think
-              that you must have a certain laundry list of accomplishments to be
-              able to do important things. Dishsoap is evidence against that. We
-              are ordinary people, striving for a true sense of freedom.
-            </p>
-            <p>In less formal terms, we really just did that. You can, too.</p>
+            <p><strong>dishsoap</strong> /ˈdɪʃˌsoʊp/ (noun) </p>
+            <p> a nerdy, uncool, earnest magazine that wouldn’t wait three days to call you if it got your 
+              number. that’s too bad at math to care about measuring accolades against one another. 
+              that doesn’t have a type beyond TNR or maybe garamond. that’s easily excitable. that can’t 
+              wait to see what you’ve been working on.</p>
           </div>
           <div className={aboutStyles.pinkLineBottom}></div>
         </div>

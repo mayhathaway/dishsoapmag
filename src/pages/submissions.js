8931dc4425137727ab7028575abc1f6ced1359c3
Kelzaam <that1poetrynerd@gmail.com>
2024-06-05 13:38:11 -0700

    Update submissions.js

--- src/pages/submissions.js
+++ src/pages/submissions.js
@@ -22,58 +22,37 @@ const Masthead = () => {
         <div className={layoutStyles.pinkLineTop}></div>
 
         <div className={layoutStyles.mission}>
-         <p> Submissions are currently closed! </p>
           <p>
             <h3>
               <i>
                 <strong>
-                  <p>Ten Commandments for Soaping</p>
+                  <p>submission guidelines</p>
                 </strong>
               </i>
             </h3>
             <ol>
-              <li>we accept all genres.</li>
               <li>
-                simultaneous submissions are fine, but tell us if you get
-                accepted elsewhere!
+                simultaneous submissions are accepted, but please notify us promptly if your work is accepted elsewhere.
+                </li>
+              <li>
+                We encourage and enjoy work that tackles tough topics, but we will reserve the right to ignore a submission if it includes anything nsfw, racist, homophobic, ableist, transphobic, sexist, or anything generally involving discrimination or hate speech.
               </li>
               <li>
-                send us your finely-crafted dishes through our <a href="https://forms.gle/nCnwkUUuZudsEAL37"> submissions form</a>. we prefer docx
-                format unless you've got some cool formatting to preserve. unfortunately, we don't have the staffing to reply to off-season submissions,
-                  or those sent by email instead of by form. cover letters are cool; please keep your bios short
-                and sweet (≤150 words) and when listing publications, please
-                list the ones that you feel are most representative of your
-                work. long bios will be cut to fit.{" "} <br />
+                Although we would love for Dishsoap to be the first place that your work has appeared in, we will accept pieces that have appeared elsewhere previously as long as you indicate that to us in your submission.
               </li>
               <li>
-                <strong>poetry.</strong> at most 4 poems per submission & no
+                <strong>poetry.</strong> max 3 poems per submission & no
                 word limit. <br />
-                <strong>prose.</strong> at most 3 per submission & 5000 words in
-                total.
-                <br />
-                <strong>art.</strong> at most 3 per submission. feel free to
-                attach a brief explanation!
-                <br />
-                <strong>other.</strong> as many as you'd like :).
+                <strong>prose.</strong> mx 2 pieces per submission & no piece should exceed 2000 words
               </li>
               <li>
-                please don't submit anything explicit!
-                <br /> <b>
-                  we reserve the right to ignore a submission if we find any
-                  nsfw, racist, homophobic, etc. notions, even if not explicity
-                  stated.
-                </b>
+              If your work is accepted, we request that you credit us when sending it to other places in the future.
               </li>
               <li>
-                if your work is accepted for publication, we ask that you credit
-                us as its first home when sending it to other places in the
-                future
+                If your work is rejected, we ask that you wait 2 months before submitting again. If your work is accepted, we ask that you wait 6 months before submitting again.
               </li>
               <li>
-                throughout the submissions window, we would prefer it if
-                dishsoap could be your pieces’ first home, but we will consider
-                work that has been housed elsewhere before. please let us know
-                in your cover letter.
+                We will generally aim to respond within two weeks. If you haven’t heard from us after three weeks, feel free to email dishsoap.mag@gmail.com with as much horrific language as you deign necessary.
               </li>
               <li>
                 we do nominate for Pushcart and Best of the Net.
@@ -81,7 +60,7 @@ const Masthead = () => {
               <li>for inquiries, please email dishsoap.mag@gmail.com. To <b>withdraw</b> a piece, 
               please edit your initial submission to <b>check the box at the bottom of the form.</b> 
               (An editable copy of your submission should be sent to you after you've filled in the form!) </li>
-              <li>thank you for trusting us.</li>
+              <li>Send your work to us through our submissions form! <a href="https://forms.gle/nCnwkUUuZudsEAL37"> submissions form</a></li>
             </ol>
           </p>
 <br />

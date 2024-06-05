0ec2789eee4a5677a302d009ca792090b7d3eb08
Kelzaam <that1poetrynerd@gmail.com>
2024-06-05 14:48:36 -0700

    Update submissions.js

--- src/pages/submissions.js
+++ src/pages/submissions.js
@@ -43,7 +43,7 @@ const Masthead = () => {
               simultaneous submissions are accepted, but please notify us promptly if your work is accepted elsewhere.
               </li>
               <li>
-                although we would love for Dishsoap to be the first place that your work has appeared in, we will accept pieces that have appeared elsewhere previously as long as you indicate that to us in your submission.
+                although we would love for dishsoap to be the first place that your work has appeared in, we will accept pieces that have appeared elsewhere previously as long as you indicate that to us in your submission.
               </li>
               <li>
               we encourage and enjoy work that tackles tough topics, but we will reserve the right to ignore a submission if it includes anything nsfw, racist, homophobic, ableist, transphobic, sexist, or anything generally involving discrimination or hate speech.

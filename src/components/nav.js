8ebcd3fc0d282de8fedb1e044658c33d20a22ce7
Kelzaam <that1poetrynerd@gmail.com>
2024-07-28 16:20:15 -0700

    Update nav.js

--- src/components/nav.js
+++ src/components/nav.js
@@ -39,7 +39,7 @@ const Nav = () => {
             <Link
               className={navStyles.navItem}
               activeClassName={navStyles.activeNavItem}
-              to="/issues"
+              to="/submissions"
             >
               submit
             </Link>

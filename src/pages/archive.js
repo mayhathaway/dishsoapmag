ca63ba253d3d08969512fcf23960d16cb0d6faee
Kelzaam <that1poetrynerd@gmail.com>
2024-07-28 19:33:21 -0700

    Update archive.js

--- src/pages/archive.js
+++ src/pages/archive.js
@@ -17,7 +17,73 @@ const Masthead = () => {
     <Subheader title="dish cabinet" />
     <Layout>
       <Head title="archive" />
+      
+      <h1 className={issueStyles.heading}>collection 5</h1>
+      <div className={layoutStyles.mission}>
+          <h1 className={issueStyles.heading}>poetry</h1>
+          <div className={issueStyles.parent}>
+            <div className={issueStyles.bottomMargin}>
 
+              <h2>howie good</h2>
+              <Link to="/issue5/the-anxiety-index">
+                <p>The Anxiety Index</p>
+              </Link>
+
+              <h2>howie good</h2>
+              <Link to="/issue5/the-day-before-yesterday">
+                <p>The Day Before Yesterday</p>
+              </Link>
+
+              <h2>jeffrey letterly</h2>
+              <Link to="/issue5/swirling">
+                <p>{`[Swirling]`}</p>
+              </Link>
+
+            </div>
+
+
+            <div className={issueStyles.bottomMargin}>
+
+              <h2>nathan lee</h2>
+              <Link to="/issue5/spring-meditation">
+                <p>spring meditation</p>
+              </Link>
+            
+              <h2>ashley kim</h2>
+              <Link to="/issue5/mom-and-dad">
+                <p>Mom and Dad</p>
+              </Link>
+
+              <h2>julieanne larick</h2>
+              <Link to="/issue5/the-anxious-girlfriends-unruly-skeletons">
+                <p>The Anxious Girlfriend's Unruly Skeletons</p>
+              </Link>
+
+            </div>
+          </div>
+
+          <h1 className={issueStyles.heading}>art</h1>
+          <div className={issueStyles.parent}>
+            <div className={issueStyles.bottomMargin}>
+
+              <h2>theresa kohlbeck jakobsen</h2>
+              <Link to="/issue5/skilnadarotti">
+                <p>skilnadarótti</p>
+              </Link>
+
+            </div>
+            <div>
+
+              <h2>james diaz</h2>
+              <Link to="/issue5/words-get-in-the-way">
+                <p>Words Get In The Way</p>
+              </Link>
+
+              </div>
+            </div>
+          </div>
+          <br></br>
+          <br></br>
       <h1 className={issueStyles.heading}> collection 4 </h1>
         <div className={layoutStyles.mission}>
             <h1 className={issueStyles.heading}>poetry</h1>

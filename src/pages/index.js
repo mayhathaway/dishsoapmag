c2bd83b854ce729366dfb4c9230dc5b94e1abee3
dishsoap-quarterly <dishsoap.mag@gmail.com>
2024-11-25 19:33:25 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -49,16 +49,16 @@ const IndexPage = () => {
           <div className={indexStyles.missiontwo}>
             <h3 className={indexStyles.postauthor}>N. J. Webster</h3>
             <h1 className={indexStyles.posttitle}> Dream 5, Year 661</h1>
-            <p>
-<break></break>
-<break></break>I used to live with a smoker who woke me up every four hours in lieu of rent, but he
-<break></break>moved out, tired of my morning shrieks, my midnight panic.
-
-<break></break>Each of my dreams lasts 1,000 years. Sometimes alone, sometimes in crawling desperate
-<break></break>slums, and sometimes here, with you. I’ve lived centuries in automated jungles and
-<break></break>villages of amber, I’ve begged and governed. I’ve seen the sun sick and the ocean a
-<break></break>sluggish violet. Sometimes I read, but the worn books contain only what I contain. And
-<break></break>sometimes I’m here, with you. And I don’t want to wake up.
+          <p>
+            <break></break>
+            <break></break>I used to live with a smoker who woke me up every four hours in lieu of rent, but he
+            <break></break>moved out, tired of my morning shrieks, my midnight panic.
+            <break></break>
+            <break></break>Each of my dreams lasts 1,000 years. Sometimes alone, sometimes in crawling desperate
+            <break></break>slums, and sometimes here, with you. I’ve lived centuries in automated jungles and
+            <break></break>villages of amber, I’ve begged and governed. I’ve seen the sun sick and the ocean a
+            <break></break>sluggish violet. Sometimes I read, but the worn books contain only what I contain. And
+            <break></break>sometimes I’m here, with you. And I don’t want to wake up.
             </p>
           </div>
           </div>

3170ae9a1a723f3bd9dea56293747d3d79228b16
dishsoap-quarterly <dishsoap.mag@gmail.com>
2025-01-22 18:33:30 -0500

    Update submissions.js

--- src/pages/submissions.js
+++ src/pages/submissions.js
@@ -12,9 +12,9 @@ const Masthead = () => {
     <>
       <MobileNav />
       <Nav />
-      <Subheader title="submissions" />
+      <Subheader title="submission guidelines" />
       <Layout>
-        <Head title="submissions" />
+        <Head title="submission"/>
         <div className={layoutStyles.title}>
           <h2>hello! thank you for considering us as a home for your work :)</h2>
         </div>
@@ -26,52 +26,89 @@ const Masthead = () => {
             <h3>
               <i>
                 <strong>
-                  <p>submission guidelines</p>
+                  <p>General Vibes</p>
                 </strong>
               </i>
             </h3>
               <li>
-              we publish one piece per week, on tuesdays. our submissions will remain perpetually open, so feel free to send us work whenever you’d like.
+              We publish one piece per week, on Tuesdays. Our submissions will remain perpetually open, so feel free to send us work whenever you’d like.
                 </li>
               <li>
-              <strong>poetry.</strong> max 3 poems per submission & no word limit. <br />
-              &emsp; &nbsp;<strong>prose.</strong> max 2 pieces per submission & no piece should exceed 1200 words
-              </li>
-              <li>
-              simultaneous submissions are accepted, but please notify us promptly if your work is accepted elsewhere.
-              </li>
-              <li>
-              we typically prefer for dishsoap to be the first place your work has appeared, but are more than happy to give a new home to work stuck in any defunct/discontinued magazine or work that's only been posted to a personal blog/social media.
-              </li>
-              <li>
-              we encourage and enjoy work that tackles tough topics, but we will reserve the right to ignore a submission if it includes anything nsfw, racist, homophobic, ableist, transphobic, sexist, or anything generally involving discrimination or hate speech.
-              </li>
-              <li>
-              if your work is accepted, you retain all rights, but we request that you credit us when sending it to other places in the future.
-              </li>
-              <li>
-              if your work is rejected, we ask that you wait 1 month before submitting again. if your work is accepted, we ask that you wait 2 months before submitting again.
-              </li>
-              <li>
-              we will generally aim to respond within one week. If you haven’t heard from us after two weeks, feel free to email dishsoap.mag@gmail.com with as much horrific language as you deign necessary.
-              </li>
-              <li>
-              to withdraw your work, please email the titles of the pieces you would like to withdraw to dishsoap.mag@gmail.com
-              </li>
-              <li>
-              we are currently not a paying market-- though we hope one day to be :)
-              </li>
-              <li>
-              we will be nominating for awards and prizes again starting in the 2025 cycle!
+                <li>
+                Typical response time is 2-4 days. All submissions will be responded to within 2 weeks.
+                </li>
+                Submissions are free! But we're also not a paying market. We hope to someday be though :)
               </li>
               <li>
                 send your work to us through our <a href="https://forms.gle/nCnwkUUuZudsEAL37"> submissions form!</a></li>
-                <li>
-              if you somehow don't have a google account yet and can't access our form, feel free to email us your submission with an optional informal cover letter and a bio.
-              </li>
           </p>
           </div>
-<br />
+
+          <div className={layoutStyles.mission}>
+          <p>
+            <h3>
+              <i>
+                <strong>
+                  <p>Genre Guidelines</p>
+                </strong>
+              </i>
+            </h3>
+            <strong>Short Fiction/Nonfiction</strong>
+            <li>
+            Tell us about that time that… and how… or an old bike that… or your next door neighbor who… or a fake guy named Ralph who loves… Up to 3 pieces / up to 1200 words per piece.
+            </li>
+          <div></div>
+            <strong>Micro Fiction/Nonfiction</strong>
+          <li>
+          Send us your little drabbles, meditations, glimpses, fortune cookie fortunes, journal entries, horoscopes, etc. Up to 4 pieces / up to 300 words per piece
+          </li>
+          <div></div>
+          <strong>Poetry/Prose Poetry</strong>
+          <li>
+          Give us your funktastic imagery and permeating voice. Up to 3 poems / no word limit.
+          </li>
+          <div></div>
+         <strong>Hybrid</strong> 
+         <li>
+          All your weird. we want it. Generally up to your discretion given the length requirements outlined for other categories. Hard limit of 4 pieces / 1200 words though.
+          </li>
+
+
+          </p>
+          </div>
+<br /><div className={layoutStyles.mission}>
+          <p>
+            <h3>
+              <i>
+                <strong>
+                  <p>Nitty Gritty</p>
+                </strong>
+              </i>
+            </h3>
+            <li>
+            We typically prefer for Dishsoap to be the first place your work has appeared, but are more than happy to give a new home to work stuck in any defunct/discontinued journal or magazine.
+            </li>          
+            <li>
+            Simultaneous submissions are accepted, but please notify us promptly if your work is accepted elsewhere.
+            </li>
+            <li>
+            We encourage and enjoy work that tackles tough topics, but we will reserve the right to ignore a submission if it includes anything nsfw, racist, homophobic, ableist, transphobic, sexist, or anything generally involving discrimination or hate speech.
+            </li>
+            <li>
+            If your work is rejected, we ask that you wait 1 month before submitting again. if your work is accepted, we ask that you wait 2 months before submitting again. 
+            </li>
+            <li>
+            To withdraw your work, email us at dishsoap.mag@gmail.com
+            </li>
+            <li> 
+            If you somehow don't have a google account yet and can't access our form, feel free to email us your submission with an optional informal cover letter and a bio.
+            </li>
+            <li>
+            If your work is accepted, you retain all rights, but we request that you credit us when sending it to other places in the future.
+            </li>
+
+          </p>
+          </div>
         <div className={layoutStyles.pinkLineBottom}></div>
       </Layout>
     </>

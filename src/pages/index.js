bf036718842ede5860cfd56e736b6b4df6293f13
dishsoap-quarterly <dishsoap.mag@gmail.com>
2025-05-07 11:31:20 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Head from "../components/head"
 import Nav from "../components/nav"
 import MobileNav from "../components/mobileNav"
 import indexStyles from "./index.module.scss"
-import mike from "../images/mike.jpeg"
+import anaphora from "../images/anaphora.jpeg"
 import soaphead from "../images/soaphead.png"
 import layoutStyles from "../components/layout.module.scss"
 import aboutStyles from "./about.module.scss"
@@ -49,49 +49,10 @@ const IndexPage = () => {
         <div className={cx(indexStyles.row, indexStyles.soapRow)}>
         <div className={cx(layoutStyles.content,indexStyles.new)}>
           <div className={indexStyles.missiontwo}>
-            <h3 className={indexStyles.postauthor}>Parker Logan</h3>
-            <h1 className={indexStyles.posttitle}>Forever Summer</h1>
+            <h3 className={indexStyles.postauthor}>Esmé Kaplan-Kinsey</h3>
+            <h1 className={indexStyles.posttitle}>Anaphora</h1>
           <p className={indexStyles.posttext}>
-          <i>Alyson Noël</i>
-          <br></br>
-          <br></br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;I don’t trust people who don’t like summer,
-          <br></br>the hottest season of the year, and maybe
-          <br></br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;it’s because I just want to take my clothes off
-          <br></br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;and rock the crop top I found at Goodwill
-          <br></br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;and a five inch inseam bather, or hit the bar
-          <br></br>wearing nothing but mesh, the warm air
-          <br></br>
-          <br></br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;of Florida, Louisiana, New York, Nevada, the whole
-          <br></br>northern hemisphere licking every inch
-          <br></br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;of my skin with sweat sweat sweat making me
-          <br></br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;wet wet wet, and I love the beach, the Atlantic,
-          <br></br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;a ride down the Ichetucknee river with a beer
-          <br></br>and an edible kicking me square in the ass
-          <br></br>
-          <br></br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;the fireworks in July popping off everywhere,
-          <br></br>something I love to watch even if I don’t
-          <br></br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;exactly enjoy celebrating the birth of the empire,
-          <br></br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;and yeah, I love kissing on New Years night--
-          <br></br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;the booms filling my ears what I really like--
-          <br></br>the lights, the colors, it’s just that the sun sets
-          <br></br>
-          <br></br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;so early in winter and rises so late I can’t help
-          <br></br>but long for the days when it’s past eight
-          <br></br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;and there’s still a little bit of sky on my face,
-          <br></br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;the heat waves on the asphalt cooling to a simmer
-          <br></br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;as we head in for dinner, which is like the summer of
-          <br></br>meals, actually, long, hearty, and hot off the grill--
-          <br></br>
-          <br></br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;oh, what I would give for a pulled pork sandwich
-          <br></br>covered in coleslaw, a cold Coca-Cola, and tater
-          <br></br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;tots underneath an air conditioner while the crickets
-          <br></br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;sing their joy at the death of winter, fear the rise
-          <br></br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;of fall, and long for spring, one degree closer
-          <br></br>to our buggy paradises and the wet, hot atmosphere
-
-
-
-
+          <picture> <img height src={anaphora}/> </picture>
 
             </p>
           </div>
@@ -100,7 +61,7 @@ const IndexPage = () => {
           <div className={indexStyles.missiontwo}>
             <p>
             <br></br>
-            Parker Logan is from Orlando, Florida and lives in Baton Rouge, Louisiana. His work has recently been featured in Voicemail Poems, Rougarou, and Barely South Review. He works as a teen library tech at the East Baton Rouge Public Library. You can read more about him at parkerpoetry.org. </p>
+            Esmé Kaplan-Kinsey is a California transplant living  in Portland, Oregon. In their writing, they hope to  explore human-nature relation and deconstruct binaries that cast humankind in opposition to the natural world. Their work appears in publications such as Adroit Journal, SmokeLong Quarterly, and the Cincinnati Review. They can be found on X/Bluesky/Instagram @esmepromise.</p>
               </div>
           </div>
         </div>

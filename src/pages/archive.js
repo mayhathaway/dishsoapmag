84aeffa7e06ee972e2378413f83a2717bacba4a9
dishsoap-quarterly <dishsoap.mag@gmail.com>
2025-06-24 17:18:50 -0400

    Update archive.js

--- src/pages/archive.js
+++ src/pages/archive.js
@@ -358,7 +358,7 @@ const Masthead = () => {
               <Link to="/6-24-25/insect/">
               <h2 className={archiveStyles.piecelink}>The Body as a Place for Lost Things</h2>
               </Link>
-              <p>aster haviland</p>
+              <p>aster havilan</p>
 
               </div>
             </div>

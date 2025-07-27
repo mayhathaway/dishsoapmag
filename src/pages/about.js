c00cdd6def368cb8028e312b843bd1d022ea9bb2
Josh Faber <josh@MacBookPro.cable.rcn.com>
2025-07-27 11:53:39 -0400

    Clean up

--- src/pages/about.js
+++ src/pages/about.js
@@ -34,7 +34,7 @@ const AboutPage = () => {
           </div>
           <div className={aboutStyles.pinkLineBottom}></div>
           <div className={aboutStyles.masthead}>
-            <Link to="/masthead">Get to know our staff here!!</Link>
+            <Link to="/masthead">Get to know our staff here!</Link>
           </div>
         </div>
       </div>

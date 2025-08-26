77c44d14f841421907b2a66afde9ffcb4b2f78ec
dishsoap-quarterly <dishsoap.mag@gmail.com>
2025-08-26 15:04:50 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -49,34 +49,34 @@ const IndexPage = () => {
             <h1 className={indexStyles.posttitle}>Smoke</h1>
           <p className={indexStyles.posttext}>
    Late summer and we are engulfed. Put your head inside
-<b></b>the black half-moon of an old charcoal grill 
-<b></b>and breathe deeply. You’ll know what I mean.
-<b></b>
-<b></b>The sun trudges weakly across the sky like an old man
-<b></b>with a dying lantern searching for lost dogs.
-<b></b>A week of this, then two. It gets to all
-<b></b>
-<b></b>of us. This gray film on everything even the stuff
-<b></b>you can’t ever see. You walk the dog and your
-<b></b>lungs send up a question: We’re sure this is a good idea?
-<b></b>
-<b></b>Yesterday my neighbor saw me
-<b></b>taking out the trash and jerked his thumb in the air
-<b></b>like, you believe this shit? He’s lived here 
-<b></b>
-<b></b>all his life he says. Third-generation Montanan. Never seen it
-<b></b>this bad. He’s got pure white hair and a kind face and
-<b></b>
-<b></b>many nights with the windows open I hear him raging
-<b></b>at the grandkids who’ve ended up living with him
-<b></b>against his will. Maybe it really is global warming, he says.
-<b></b>
-<b></b>Or climate change, he says, whatever they’re calling it now.
-<b></b>Course, he says, maybe it’s also just bad luck. He knows
-<b></b>that can happen too. It’s one of those things they always
-<b></b>
-<b></b>forget to tell you when they’re trying so very hard to get you 
-<b></b>to think about what you’re doing in your life.
+<br></br>the black half-moon of an old charcoal grill 
+<br></br>and breathe deeply. You’ll know what I mean.
+<br></br>
+<br></br>The sun trudges weakly across the sky like an old man
+<br></br>with a dying lantern searching for lost dogs.
+<br></br>A week of this, then two. It gets to all
+<br></br>
+<br></br>of us. This gray film on everything even the stuff
+<br></br>you can’t ever see. You walk the dog and your
+<br></br>lungs send up a question: We’re sure this is a good idea?
+<br></br>
+<br></br>Yesterday my neighbor saw me
+<br></br>taking out the trash and jerked his thumb in the air
+<br></br>like, you believe this shit? He’s lived here 
+<br></br>
+<br></br>all his life he says. Third-generation Montanan. Never seen it
+<br></br>this bad. He’s got pure white hair and a kind face and
+<br></br>
+<br></br>many nights with the windows open I hear him raging
+<br></br>at the grandkids who’ve ended up living with him
+<br></br>against his will. Maybe it really is global warming, he says.
+<br></br>
+<br></br>Or climate change, he says, whatever they’re calling it now.
+<br></br>Course, he says, maybe it’s also just bad luck. He knows
+<br></br>that can happen too. It’s one of those things they always
+<br></br>
+<br></br>forget to tell you when they’re trying so very hard to get you 
+<br></br>to think about what you’re doing in your life.
 
             </p>
           </div>

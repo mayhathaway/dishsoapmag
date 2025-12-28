db869210af094478fc549def96d6f8fd034ebb54
dishsoap-quarterly <dishsoap.mag@gmail.com>
2025-12-28 12:52:26 -0800

    Update masthead.js

--- src/pages/masthead.js
+++ src/pages/masthead.js
@@ -43,19 +43,6 @@ const Masthead = () => {
             </div>
           </div>
 
-
-          <div className={mastheadStyles.div6}>
-            <div className={mastheadStyles.flex}>
-              <img src={cheese} alt="website logo" />
-              <div className={mastheadStyles.badges}>
-                <div className={mastheadStyles.name}>aster haviland</div>
-                <div className={mastheadStyles.role}>editor</div>
-              </div>
-            </div>
-            <div className={mastheadStyles.description}>
-              Aster (they/them) is a senior at Princeton University studying Comparative Literature and always questioning their life decisions. They enjoy dogs, robots, mermaids, and learning new languages. Their work has been published in Okay Donkey, Ninth Letter Web Issue, and the Nassau Review.
-            </div>
-          </div>
         </div>
         <div className={mastheadStyles.role}>
           currently, there are no open positions on staff.

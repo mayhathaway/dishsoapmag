00d988a8cef7ce0eeb28be118fbda5deb1237933
dishsoap-quarterly <dishsoap.mag@gmail.com>
2026-04-08 22:56:01 -0400

    masthead bios and stuff

--- src/pages/masthead.js
+++ src/pages/masthead.js
@@ -76,36 +76,36 @@ const Masthead = () => {
             <div className={mastheadStyles.flex}>
               <img src={cam} alt="website logo" />
               <div className={mastheadStyles.badges}>
-                <div className={mastheadStyles.name}>Name</div>
-                <div className={mastheadStyles.role}>Poetry Reader</div>
+                <div className={mastheadStyles.name}>Cameron Vernali</div>
+                <div className={mastheadStyles.role}>Prose Reader</div>
               </div>
             </div>
             <div className={mastheadStyles.description}>
-            Ben lives in Los Angeles with his wife, a high school teacher, and three extremely powerful daughters. A proud member of the Bursky Collective, Ben's work has been featured or is forthcoming in Bending Genres, Bruiser Mag, Dodo Eraser, Gone Lawn, HAD, Maudlin House, Scaffold and other weird journals. Find more of his work on X @benjaminstarr and at <a href="https://benstarrwrites.com"> benstarrwrites.com</a>
+            Cameron Vernali is a writer and amateur puzzle maker. She wrote on a short-form branded web series through Say Studios and was a Stowe Narrative Labs Fellowship Finalist. She has been published in The Dry River, Syllabus and more. She takes advantage of living in California by hiking and camping often.</a>
             </div>
           </div>
       <div className={mastheadStyles.div4}>
             <div className={mastheadStyles.flex}>
               <img src={mj} alt="website logo" />
               <div className={mastheadStyles.badges}>
-                <div className={mastheadStyles.name}>Name</div>
+                <div className={mastheadStyles.name}>M.J. Young</div>
                 <div className={mastheadStyles.role}>Poetry Reader</div>
               </div>
             </div>
             <div className={mastheadStyles.description}>
-            Trinity Richardson (they/she) is a poet raising an evil cat. They have been nominated for the Pushcart Prize. Their poems can be found or are forthcoming in Gulf Stream, Moon City Review, Thimble, and more. Outside of writing, their interests include Magic, magic, claw machines, stories with ambiguous endings, and the fly from Breaking Bad.
+            M.J. Young (he/him) is a writer and MFA student at Florida International University, where he is a graduate instructor. His poetry can be found in Ninth Letter, phoebe, The Penn Review, and elsewhere. In his free time he enjoys listening to Philip Glass and exploring bookstores.
             </div>
           </div>
         <div className={mastheadStyles.div5}>
             <div className={mastheadStyles.flex}>
               <img src={lillie} alt="website logo" />
               <div className={mastheadStyles.badges}>
-                <div className={mastheadStyles.name}>Name</div>
-                <div className={mastheadStyles.role}>Poetry Reader</div>
+                <div className={mastheadStyles.name}>Lillie E. Franks</div>
+                <div className={mastheadStyles.role}>Prose Reader</div>
               </div>
             </div>
             <div className={mastheadStyles.description}>
-            Ben lives in Los Angeles with his wife, a high school teacher, and three extremely powerful daughters. A proud member of the Bursky Collective, Ben's work has been featured or is forthcoming in Bending Genres, Bruiser Mag, Dodo Eraser, Gone Lawn, HAD, Maudlin House, Scaffold and other weird journals. Find more of his work on X @benjaminstarr and at <a href="https://benstarrwrites.com"> benstarrwrites.com</a>
+            Lillie E. Franks is an author and educator who lives in Chicago, Illinois, but is normal about it. You can read her work at places like Flash Frog, Hex, and HAD or follow her on Bluesky at @lilliekoi.bsky.social. She loves anything that is not the way it should be. </a>
             </div>
           </div>
         </div>

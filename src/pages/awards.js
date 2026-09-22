e2bd4f0cfa055427cf09d0974ff89a4b37aba3c3
dishsoap-quarterly <dishsoap.mag@gmail.com>
2026-09-22 11:17:53 -0400

    Revert "Update awards.js"
    This reverts commit c2bd16ed44e762401b8962d54cbac4679e2a1d9d.

--- src/pages/awards.js
+++ src/pages/awards.js
@@ -26,7 +26,88 @@ const Masthead = () => {
           <div className={issueStyles.parent}>
             <div className={issueStyles.bottomMargin}>
 
- 
+ <Link to="/6-30-26">
+<h2 className={archiveStyles.piecelink}>Lua</h2>
+            </Link>
+            <p>lane devers</p>
+
+             <Link to="/9-9-25/mars/">
+                        <h2 className={archiveStyles.piecelink}>On Mars</h2>
+                        </Link>
+                        <p>c. rivera</p>
+
+                  <Link to="/3-10-26">
+                  <h2 className={archiveStyles.piecelink}>Closure</h2>
+                              </Link>
+                              <p>ashley varela</p>
+
+                        <Link to="/11-25-25">
+                                    <h2 className={archiveStyles.piecelink}>fossils</h2>
+                                    </Link>
+                                    <p>k.m. hanslik</p>      
+
+                     <Link to="/6-23-26">
+                    <h2 className={archiveStyles.piecelink}>Waiting on a Dream</h2>
+                                </Link>
+                                <p>tara manshon</p>
+
+                                <Link to="/1-20-26">
+                                <h2 className={archiveStyles.piecelink}>The Great Metro Detroit Flood</h2>
+                                            </Link>
+                                            <p>shea socrates</p>
+
+                  <Link to="/2-3-26">
+                  <h2 className={archiveStyles.piecelink}>Kijiji Couch</h2>
+                   </Link>
+                   <p>lindsay mcdonald</p>
+
+                     <Link to="/6-16-26">
+                   <h2 className={archiveStyles.piecelink}>My cat sits on me, incubating Sad-Away</h2>
+                               </Link>
+                               <p>thanh bui</p>
+
+
+              </div>
+            </div>
+          </div>
+          <br></br>
+          <br></br>
+
+ <h1 className={issueStyles.heading}>Best Small Fictions Nominees 2027</h1>
+      <div className={layoutStyles.mission}>
+          <div className={issueStyles.parent}>
+            <div className={issueStyles.bottomMargin}>
+
+         <Link to="/7-7-26">
+        <h2 className={archiveStyles.piecelink}>Mom's Bigfoot</h2>
+                    </Link>
+                    <p>ashley w. cundiff</p>
+
+                     <Link to="/7-14-26">
+                    <h2 className={archiveStyles.piecelink}>Pull It Together, Wilson</h2>
+                                </Link>
+                                <p>emily rinkema</p>
+
+          <Link to="/5-19-26">
+          <h2 className={archiveStyles.piecelink}>Paul From the Future</h2>
+                      </Link>
+                      <p>sarp sozdinler</p>
+
+           <Link to="/4-7-26">
+           <h2 className={archiveStyles.piecelink}>Things I thought about while driving across the country</h2>
+                                  </Link>
+                                  <p>nick van osdol</p>
+
+          <Link to="/4-21-26">
+         <h2 className={archiveStyles.piecelink}>The Girlfriends Of The Propeller Plane Disappearance Victims</h2>
+           </Link>
+           <p>dallon robinson</p>
+
+              </div>
+            </div>
+          </div>
+          <br></br>
+          <br></br>
 
       <h1 className={issueStyles.heading}>Best of the Net Nominees 2027</h1>
       <div className={layoutStyles.mission}>

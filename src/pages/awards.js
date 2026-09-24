5453fb42e7f2d9ee7283bcb0ae638fa7cc10f7f7
dishsoap-quarterly <dishsoap.mag@gmail.com>
2026-09-24 18:41:31 -0400

    Update awards.js

--- src/pages/awards.js
+++ src/pages/awards.js
@@ -10,7 +10,6 @@ import issueStyles from "./issues.module.scss"
 import { Link } from "gatsby"
 import archiveStyles from "./archive.module.scss"
 import indexStyles from "./index.module.scss" 
-import soaphead from "../images/soaphead.png"
 
 const Masthead = () => {
   return (
